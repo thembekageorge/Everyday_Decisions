a70c9d78628c19f225244423222d2c68f31c3b1f
thembekageorge <thembiegeorge@gmail.com>
2017-05-19 12:37:33 +0200

    ..

--- ed.js
+++ ed.js
@@ -1,232 +1,123 @@
+
 (function() {
+    'use strict';
 
-  'use strict';
-  
-      function initTemplate(templateSelector) {
-          var templateText = document.querySelector(templateSelector).innerHTML;
-          return Handlebars.compile(templateText);
-      }
+    function initTemplate(templateSelector) {
+        var templateText = document.querySelector(templateSelector).innerHTML;
+        return Handlebars.compile(templateText);
+    }
 
-      // var filterTemplate = initTemplate('.filterTemplate');
-      // var filters = document.querySelector('.filters');
+    var addRecipeTemplate = initTemplate('.AddRecipeTemplate');
+    var recipesElement = document.querySelector('.recipesList');
 
-      // var recipeTemplate = initTemplate('.recipeTemplate');
-      // var recipesElement = document.querySelector('.recipeList');
+    var viewAddDivButton = document.querySelector('.viewAdd');
+    var viewRecipesButton = document.querySelector('.viewView');
 
-      var viewAdd = document.querySelector('.viewAdd');
+    var addButton = document.querySelector('.FinalAdd');
 
-      viewAdd.addEventListener('click',function() {
-           var getDiv = document.querySelector('.addRecipeDiv');
+    var recipeTitle = document.querySelector('.recipeTitle');
+    var recipeIngredients = document.querySelector('.recipeIngredients');
+    var recipeServe = document.querySelector('.recipeServe');
+    var recipeInstructions = document.querySelector('.recipeInstructions');
 
-            if (getDiv.style.display === 'none')
-            {
-            getDiv.style.display = 'block';
-            }
-            else {
-            getDiv.style.display = 'none';
-            }
-       });
+
+    var recipes = [{
+    recipeTitle : 'Curry Pasta Salad',
+    recipeIngredients : '500 g pasta screws' +
+'65 ml (¼ C) sunflower oil' +
+'1 green pepper, seeds removed, chopped' +
+'1 red onion, chopped'+
+'1 x 240 g Rhodes Tomato Puree Cup' +
+'125 ml (½ C) vinegar' +
+'15 – 30 ml (1 – 2 T) curry powder' +
+'5 ml (1 t) ground turmeric' +
+'125 ml (½ C) Rhodes Apricot Jam Superfine' +
+'salt and freshly ground black pepper'+
+'65 ml (¼ C) chopped parsley',
+    recipeServe : 6,
+    recipeInstructions : 'Cook the pasta screws as per the packet instructions and set aside to cool'
++ 'Heat the half of the oil in a medium saucepan.'
++ 'Add the green pepper and the onion and fry until softened but not browned.'
++ 'Lower the heat and add the Rhodes Tomato Puree Cup, remaining oil and vinegar.'
++ 'Bring to the boil.'
++ 'Add the curry powder, tumeric and the Rhodes Apricot Jam Superfine.'
++ 'Whisk well to mix and continue to cook over a low heat for 5 minutes.'
++ 'Season to taste with the salt and freshly ground black pepper.'
++ 'Pour the curry sauce over the noodles and toss to mix.'
++'Add the parsley.'
++'Chill until serving.'
+    }];
+
+    function display(data) {
+
+        var recipeTitle = [];
+        var recipeIngredients = [];
+        var recipeServe = [];
+        var recipeInstructions = [];
+
+        for (var i = 0; i < data.length; i++) {
+            var recipesData = data[i];
+
+        }
+
+
+        recipesElement.innerHTML = addRecipeTemplate({
+            recipes: recipesData
+        });
+}
+viewAddDivButton.addEventListener('click', function() {
+      var getDiv = document.querySelector('.addRecipeDiv');
+
+       if (getDiv.style.display === 'none')
+       {
+       getDiv.style.display = 'block';
+       }
+       else {
+       getDiv.style.display = 'none';
+       }
+  });
+
+  viewRecipesButton.addEventListener('click', function() {
+        var getDiv = document.querySelector('.display');
+
+         if (getDiv.style.display === 'none')
+         {
+         getDiv.style.display = 'block';
+         }
+         else {
+         getDiv.style.display = 'none';
+         }
+    });
+
+    addButton.addEventListener('click', function() {
+        var recipeTitleValue = recipeTitle.value;
+        var recipeIngredientsValue = recipeIngredients.value;
+        var recipeServeValue = recipeServe.value;
+        var recipeInstructionsValue = recipeInstructions.value;
+
+
+        if (recipeTitleValue !== ''
+            && recipeIngredientsValue !== ''
+            && recipeServeValue !== ''
+            && recipeInstructionsValue !== '') {
+            boots.push({
+              recipeTitle : recipeTitleValue,
+              recipeIngredients :recipeIngredientsValue,
+              recipeServe : recipeServeValue,
+              recipeInstructions : recipeInstructionsValue,
+            });
+
+        }
+
+        display(recipes);
+
+        recipeTitle.value = "";
+        recipeIngredients.value = "";
+        recipeServe.value = "";
+        recipeInstructions.value = "";
+
+    });
+
+    display(recipes);
+
 })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-// (function() {
-//     'use strict';
-//
-//     function initTemplate(templateSelector) {
-//         var templateText = document.querySelector(templateSelector).innerHTML;
-//         return Handlebars.compile(templateText);
-//     }
-//
-//     var filterTemplate = initTemplate('.filterTemplate');
-//     var filters = document.querySelector('.filters');
-//
-//     var bootsTemplate = initTemplate('.bootsTemplate');
-//     var bootsElement = document.querySelector('.bootsList');
-//
-//     var addStockButton = document.querySelector('.addBootStock');
-//     var showAvailableStock = document.querySelector('.showAvailableBoots');
-//
-//     var addButton = document.querySelector('.addButton');
-//     var filterButton = document.querySelector('.filterButton');
-//
-//     var bootName = document.querySelector('.bootName');
-//     var bootColor = document.querySelector('.bootColor');
-//     var bootSize = document.querySelector('.bootSize');
-//     var bootPrice = document.querySelector('.bootPrice');
-//     var quantity = document.querySelector('.quantity');
-//
-//
-//     var boots = [{
-//     bootName : 'boot1',
-//     bootColor : 'pink',
-//     bootSize : 4,
-//     bootPrice :"R"+400,
-//     quantity : 3
-//     }];
-//
-//     function display(data, bootsList) {
-//
-//  if (!bootsList){
-//
-//    bootsList = data;
-//  }
-//         var bootNames = [];
-//         var bootColors = [];
-//         var bootSizes = [];
-//         var bootPrices = [];
-//         var quantities = [];
-//
-// var bootMap = {};
-// for (var i = 0; i < data.length; i++) {
-//     var bootsData = data[i];
-//     if (bootMap[bootsData.bootName] === undefined){
-//       bootMap[bootsData.bootName] = bootsData.bootName;
-//       bootNames.push(bootsData.bootName);
-//     }
-//     if (bootMap[bootsData.bootColor] === undefined){
-//       bootMap[bootsData.bootColor]= bootsData.bootColor;
-//       bootColors.push(bootsData.bootColor);
-//     }
-//     if (bootMap[bootsData.bootSize] === undefined){
-//       bootMap[bootsData.bootSize] = bootsData.bootSize;
-//       bootSizes.push(bootsData.bootSize);
-//     }
-//     if (bootMap[bootsData.bootPrice] === undefined){
-//       bootMap[bootsData.bootPrice] = bootsData.bootPrice;
-//       bootPrices.push(bootsData.bootPrice);
-//     }
-//   }
-//         for (var i = 0; i < data.length; i++) {
-//             var bootsData = data[i];
-//
-//         }
-//
-//         filters.innerHTML = filterTemplate({
-//             bootNames: bootNames.sort(),
-//             bootColors  : bootColors.sort(),
-//             bootSizes  : bootSizes.sort(),
-//             bootPrices  : bootPrices.sort(),
-//             quantities  : quantities.sort(function(a,b){
-//                 return a - b;
-//             })
-//         });
-//
-//         bootsElement.innerHTML = bootsTemplate({
-//             boots: bootsList
-//         });
-// }
-// //When button 'Add Stock' is clicked, this will be so that the user
-// // has access to the div that allows them to add stock
-// addStockButton.addEventListener('click', function() {
-//       var getDiv = document.querySelector('.add');
-//
-//        if (getDiv.style.display === 'none')
-//        {
-//        getDiv.style.display = 'block';
-//        }
-//        else {
-//        getDiv.style.display = 'none';
-//        }
-//   });
-//
-//   showAvailableStock.addEventListener('click', function() {
-//         var getDiv = document.querySelector('.display');
-//
-//          if (getDiv.style.display === 'none')
-//          {
-//          getDiv.style.display = 'block';
-//          }
-//          else {
-//          getDiv.style.display = 'none';
-//          }
-//     });
-//
-//     filters.addEventListener('click', function(evt){
-//         if (evt.target.name === 'filterButton'){
-//
-//             var bootNameFilter = document.querySelector('.bootNameFilter');
-//             var bootColorFilter = document.querySelector('.bootColorFilter');
-//             var bootSizeFilter = document.querySelector('.bootSizeFilter');
-//             var bootPriceFilter = document.querySelector('.bootPriceFilter');
-//
-//   var filteredboots = [];
-//             if (bootNameFilter.value == ""
-//             && bootColorFilter.value == ""
-//             && bootSizeFilter.value == ""
-//             && bootPriceFilter.value == "")
-//             {
-//               display(boots);
-//               return;
-//             }
-//
-//
-//             for (var i = 0; i < boots.length; i++) {
-//               var boot = boots[i]
-//               if (boot.bootName === bootNameFilter.value){
-//                 filteredboots.push(boot);
-//               }
-//               if (boot.bootColor === bootColorFilter.value){
-//                 filteredboots.push(boot);
-//               }
-//               if (boot.bootSize === bootSizeFilter.value){
-//                 filteredboots.push(boot);
-//               }
-//               if (boot.bootPrice === bootPriceFilter.value){
-//                 filteredboots.push(boot);
-//               }
-//             }
-//             display(boots, filteredboots);
-//         }
-//     });
-//
-//     addButton.addEventListener('click', function() {
-//         var bootNameValue = bootName.value;
-//         var bootColorValue = bootColor.value;
-//         var bootSizeValue = bootSize.value;
-//         var bootPriceValue = bootPrice.value;
-//         var quantityValue = quantity.value;
-//
-//
-//         if (bootNameValue !== ''
-//             && bootColorValue !== ''
-//             && bootSizeValue !== ''
-//             && bootPriceValue !== ''
-//              && quantityValue !== '') {
-//             boots.push({
-//               bootName : bootNameValue,
-//               bootColor :bootColorValue,
-//               bootSize : bootSizeValue,
-//               bootPrice :"R"+bootPriceValue,
-//               quantity : quantityValue
-//             });
-//
-//         }
-//
-//         display(boots);
-//
-//         bootName.value = "";
-//         bootColor.value = "";
-//         bootSize.value = "";
-//         bootPrice.value = "";
-//         quantity.value = "";
-//     });
-//
-//     display(boots);
-//
-// })();
